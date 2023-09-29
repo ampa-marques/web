e515e1e3795be314dca98e8ee3592763579f7db8
plutomon <55143679+plutomon@users.noreply.github.com>
2023-09-29 11:42:37 +0200

    Actualizado enlace MATINERA al icono

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -424,6 +424,10 @@ export default function Index({ posts, tags, pagination }: Props) {
                                 className="h-icon style-178 style-local-161-c9 position-relative h-element"
                               >
                                 {/**/}
+                                 <a
+                                  href="/matinera"
+                                  h-use-smooth-scroll="true"
+                                >
                                 <span className="h-svg-icon h-icon__icon style-178-icon style-local-161-c9-icon">
                                   {/*Icon by Font Awesome (https://fontawesome.com)*/}
                                   <svg
@@ -436,6 +440,7 @@ export default function Index({ posts, tags, pagination }: Props) {
                                     <path d="M1472 896q0-117-45.5-223.5t-123-184-184-123T896 320t-223.5 45.5-184 123-123 184T320 896t45.5 223.5 123 184 184 123T896 1472t223.5-45.5 184-123 123-184T1472 896zm276 277q-4 15-20 20l-292 96v306q0 16-13 26-15 10-29 4l-292-94-180 248q-10 13-26 13t-26-13l-180-248-292 94q-14 6-29-4-13-10-13-26v-306l-292-96q-16-5-20-20-5-17 4-29l180-248L48 648q-9-13-4-29 4-15 20-20l292-96V197q0-16 13-26 15-10 29-4l292 94L870 13q9-12 26-12t26 12l180 248 292-94q14-6 29 4 13 10 13 26v306l292 96q16 5 20 20 5 16-4 29l-180 248 180 248q9 12 4 29z" />
                                   </svg>
                                 </span>
+                                   </a>
                               </div>
                               <div
                                 data-colibri-id="161-c10"
